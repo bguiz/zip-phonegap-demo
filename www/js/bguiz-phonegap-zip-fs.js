44fd44db13a275c87917484bb3167449c88aa319
bguiz <brendan.graetz@gmail.com>
2015-06-02 12:59:54 +1000

    =BG= create an `extractZipToFolder` which combines `extractZip` and `writeFile` functionality

--- www/js/bguiz-phonegap-zip-fs.js
+++ www/js/bguiz-phonegap-zip-fs.js
@@ -3,26 +3,96 @@
 var MAX_CONCURRENT_INFLATE = 512;
 var MAX_CONCURRENT_SIZE_COST = 2 * 1024 * 1024;
 
+function onFail(err) {
+  console.log(err);
+  throw err;
+}
+
 function writeOutMessage(message) {
   var elem = document.getElementById('out');
   elem.appendChild(document.createTextNode(message));
   elem.appendChild(document.createElement('br'));
 }
 
+/**
+ * Opens a Zip file, and inflates all of its contents to a folder on the filesystem
+ * Combines `extractZip` and `writeFile` functionality,
+ * adding a management layer.
+ *
+ * @param  {Object} options
+ *   {
+ *     useWebWorkers: false,
+ *     workerScripts: {},
+ *     readerUrl: '',
+ *     extractFolder: '',
+ *   }
+ * @param  {Function} onDone  [description]
+ */
+function extractZipToFolder(options, onDone) {
+  var numFiles = 0;
+  var numFilesWritten = 0;
+  var numFilesErrored = 0;
+  var allInflated = false;
+  // Modify writer options to an intermediate format of this function's preference
+
+  var zipOptions = {
+    useWebWorkers: options.useWebWorkers,
+    workerScripts: options.workerScripts,
+    readerType: 'HttpReader',
+    readerUrl: options.readerUrl,
+    writerType: 'BlobWriter',
+  };
+  extractZip(zipOptions, function onExtractZipDone(err, allDone, fileInfo) {
+    if (!!err) {
+      throw err;
+    }
+    if (!allDone) {
+      // Signalled that a single file in the zip file has been inflated, and here it is
+
+      // Modify writer options to write to the file system
+      var fileOptions = {
+        name: options.extractFolder+'/'+fileInfo.fileEntry.filename,
+        flags: {
+          create: true,
+          exclusive: false,
+          mkdirp: true,
+        },
+        blob: fileInfo.contents,
+      };
+      ++numFiles;
+      writeFile(fileOptions, function onWriteFileDone(err, evt) {
+        if (!!err) {
+          ++numFilesErrored;
+          onFail(err);
+        }
+        ++numFilesWritten;
+        console.log('File written:', fileInfo, 'numFilesWritten:', numFilesWritten);
+        checkComplete();
+      });
+    }
+    else {
+      // Signalled that all files in the zip file have been inflated
+      allInflated = true;
+      checkComplete();
+    }
+    function checkComplete() {
+      if (allInflated && numFilesWritten + numFilesErrored >= numFiles) {
+        onDone(undefined, numFiles);
+      }
+    }
+  });
+}
+
 function extractZip(options, onDone) {
   zip.useWebWorkers = options.useWebWorkers;
   zip.workerScripts = options.workerScripts;
-  var reader;
-  switch (options.readerType) {
-    case 'HttpReader':
-      reader = new zip.HttpReader(options.readerUrl);
-      break;
-    default:
-      throw 'Unrecognised zip reader type: '+options.readerType;
-  }
+  var reader = getZipReader(options);
 
   zip.createReader(reader, function onZipReaderCreated(zipReader) {
     zipReader.getEntries(function onZipEntriesListed(entries) {
+      entries = entries.filter(function(entry) {
+        return !entry.directory;
+      });
       console.log(options.name, 'entries.length:', entries.length);
       var resultCount = entries.length;
       var concurrentEntries = 0;
@@ -40,17 +110,10 @@ function extractZip(options, onDone) {
         ++concurrentEntries;
         concurrentCost += estimatedSizeCost;
 
-        var writer;
-        switch (options.writerType) {
-          case 'Data64URIWriter':
-            writer = new zip.Data64URIWriter(zip.getMimeType(entry.fileName));
-            break;
-          default:
-            throw 'Unrecognised zip writer type: '+options.writerType;
-        }
+        var writer = getZipWriter(options, entry);
         entry.getData(writer, function onGotDataForZipEntry(data) {
           onDone(undefined, false, {
-            fileName: entry.fileName,
+            fileEntry: entry,
             contents: data,
           });
 
@@ -94,18 +157,26 @@ function extractZip(options, onDone) {
 function getDataFile(path, options, onGotFileEntry) {
   options = options || {};
   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function onGotFileSytem(fileSys) {
-    console.log("got main fileSys", fileSys);
-    fileSys.root.getFile(path, options, function(fileEntry) {
-      console.log("got the fileEntry", fileEntry);
-      onGotFileEntry(undefined, fileEntry);
-    }, function onFail(err) {
-      console.log('Err', err);
-      throw err;
-    });
-  }, function onFail(err) {
-    console.log('Err', err);
-    throw err;
-  });
+    // console.log('fileSys:', fileSys);
+    if (options.mkdirp) {
+      var dirPath = path.replace( /\/[^\/]+$/ , '');
+      mkdirp(fileSys.root, dirPath, function onMkdirpDone(err, dirEntry) {
+        if (!!err) {
+          onFail(err);
+        }
+        continueGetDataFile();
+      });
+    }
+    else {
+      continueGetDataFile();
+    }
+    function continueGetDataFile() {
+      fileSys.root.getFile(path, options, function(fileEntry) {
+        // console.log('fileEntry:', fileEntry);
+        onGotFileEntry(undefined, fileEntry);
+      }, onFail);
+    }
+  }, onFail);
 }
 
 function writeFile(options, onDone) {
@@ -114,7 +185,16 @@ function writeFile(options, onDone) {
       onFail(err);
     }
     fileEntry.createWriter(function onWriterCreated(writer) {
-      var blob = new Blob([options.content], { type: options.mimeType });
+      var blob;
+      if (options.blob) {
+        blob = options.blob;
+      }
+      else if (options.content && options.mimeType) {
+        blob = new Blob([options.contents], { type: options.mimeType });
+      }
+      else {
+        throw 'Cannot create file, invalid options';
+      }
       writer.onwriteend = onWrote;
       writer.write(blob);
 
@@ -149,4 +229,96 @@ function readFile(options, onDone) {
       }
     }, onFail);
   }, onFail);
+}
+
+function mkdirp(fsRoot, path, onDone) {
+    var dirs = path.split('/').reverse();
+
+    function mkdir(dir) {
+      // console.log('mkdir', dir);
+      fsRoot.getDirectory(dir, {
+        create : true,
+        exclusive : false,
+      },  onCreateDirSuccess, onCreateDirFailure);
+    }
+
+    function mkdirSub() {
+      if (dirs.length > 0) {
+        mkdir(dirs.pop());
+      } else {
+        console.log('mkdir -p OK', path);
+        onDone(undefined, path);
+      }
+    }
+
+    function onCreateDirSuccess(dirEntry) {
+      // console.log('mkdir OK ', dirEntry.fullPath);
+      fsRoot = dirEntry;
+      mkdirSub();
+    }
+
+    function onCreateDirFailure(err) {
+      // TODO start doing some form of optimisation like this
+      // if (!!err && err.code === FileError.PATH_EXISTS_ERR) {
+      //   // We can safely ignore any errors that occur
+      //   // as a result of the directory already existing.
+      //   //
+      //   // In fact, we use `exclusive: true`to intentionally trip up this error
+      //   // in order to shortcut to safety to performance optimisation reasons.
+      //   mkdirSub();
+      // }
+      // else {
+      //   console.log('mkdir fail', err, !!err && err.stack);
+      //   throw err;
+      // }
+      console.log('mkdir fail', err, !!err && err.stack);
+      throw err;
+    }
+
+    mkdir(dirs.pop());
+}
+
+function getZipReader(options) {
+  var reader;
+  switch (options.readerType) {
+    case 'TextReader':
+      reader = new zip.TextReader(options.readerText);
+      break;
+    case 'BlobReader':
+      reader = new zp.BlobReader(options.readerBlob);
+      break;
+    case 'Data64URIReader':
+      reader = new zip.Data64URIReader(options.readerDataUri);
+      break;
+    case 'HttpReader':
+      reader = new zip.HttpReader(options.readerUrl);
+      break;
+    case 'HttpRangeReader':
+      reader = new zip.HttpRangeReader(options.readerUrl);
+      break;
+    default:
+      throw 'Unrecognised zip reader type: '+options.readerType;
+  }
+  return reader;
+}
+
+function getZipWriter(options, fileEntry) {
+  var writer;
+  switch (options.writerType) {
+    case 'TextWriter':
+      writer = new zip.TextWriter();
+      break;
+    case 'BlobWriter':
+      writer = new zip.BlobWriter(zip.getMimeType(fileEntry.fileName));
+      break;
+    case 'FileWriter':
+      writer = new zip.FileWriter(fileEntry);
+      break;
+    case 'Data64URIWriter':
+      writer = new zip.Data64URIWriter(zip.getMimeType(fileEntry.fileName));
+      break;
+    default:
+      throw 'Unrecognised zip writer type: '+options.writerType;
+  }
+  return writer;
 }
